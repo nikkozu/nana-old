1f7008cd6854c051dd28a6eed1b220ac11bef6db
Samuel Lekatompessy <45459712+lekatompessy@users.noreply.github.com>
2019-06-28 13:08:26 +0700

    Update language.js

--- commands/language.js
+++ commands/language.js
@@ -6,24 +6,27 @@ exports.run = async (client, msg, args, color) => {
     if (!msg.channel.nsfw) return msg.channel.send(`NSFW channel please.`).then(msg => msg.delete(5000));
   
     let lang = args[0];
-    if (lang != 'english' && lang != 'japanese' && lang != 'chinese') return msg.channel.send('Available langauge is \`English\`, \`Japanese\` & \`Chinese\`').then(msg => msg.delete(5000));
-    
+    if (lang == "ch") {
+        lang = "chinese"
+    } else if (lang == 'en') {
+        lang = 'english'
+    } else if (lang == 'jp') {
+        lang = 'japanese'
+    }
+    if (!client.config.LANG.includes(lang)) return msg.channel.send('Available langauge is \`English\`, \`Japanese\` & \`Chinese\`').then(msg => msg.delete(5000));
+  
     let numPages = await api.search(lang);
     let id = await api.search(lang, client.util.getRandInt(numPages.num_pages));
     const res = await api.g(id.results[client.util.getRandInt(id.results.length)].id);
-    let m = await msg.channel.send(client.embeds.getInfoEmbed(res));
-  
-    client.embeds.getEmoji(res, m, msg);
-    // msg.channel.send(`https://nhentai.net/g/${res.results[client.util.getRandInt(res.results.length)].id}`);
+    await client.embeds.getInfoEmbed(res.id, msg);
 };
 
 exports.conf = {
-  aliases: ['lang'],
-  cooldown: '15'
+  aliases: ['lang']
 }
 
 exports.help = {
   name: "language",
-  description: 'Search random nHentai manga from language.',
+  description: 'Search random nHentai manga from language. You can use alias language\nExample: en, ch, or jp.',
   usage: 'language'
 }
